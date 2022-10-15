27e5e557738e55dab18e1cb2b483a268896ea54e
Ronny Nordelo Peña <ronnynordelopena@gmail.com>
2022-10-15 08:31:36 -0400

    First possible final result

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-$(".nav").load('#nav_pc.html');
-
+$(function () {
+  var scroll=$(document).scrollTop();
+  var navHeight=$('#navId').outerHeight();
+  $(window).scroll(function () {
+    var scrolled=$(document).scrollTop();
+    if(scrolled>navHeight){
+      $('#navId').addClass('animate');
+    }else{
+      $('#navId').removeClass('animate');
+    }
+    if(scrolled>scroll){
+      $('#navId').removeClass('sticky');
+    }else{
+      $('#navId').addClass('sticky');
+    }
+    scroll=$(document).scrollTop();
+  });
+});
